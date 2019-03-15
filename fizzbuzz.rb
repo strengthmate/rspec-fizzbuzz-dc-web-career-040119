abf5174a58ab1794222e66aece12ee30fd92d3b8
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-15 17:06:47 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,19 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-require 'pry'
 
 def fizzbuzz (n)
-
   if n % 3 == 0
-    'Fizz'
-elsif n % 5 == 0
+    return 'Fizz'
+  end
+  
+  if n % 5 == 0
     return 'Buzz'
-elsif n % 3 == 0 || n % 5 == 0
+  end
+  
+  if n % 3 == 0 || n % 5 == 0
     return 'FizzBuzz'
-else n % 3 != 0 || n % 5 != 0
+  end
+    
+  if n % 3 != 0 || n % 5 != 0
     return nil
   end
