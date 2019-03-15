faa43e92629b270db63a13480f525d973bc7e43a
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-15 16:56:30 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,15 +2,15 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz (n)
-  if n % 3 = 0
+  if n % 3 == 0
     return 'Fizz'
   end
   
-  if n % 5 = 0
+  if n % 5 == 0
     return 'Buzz'
   end
   
-  if n % 3 = 0 || n % 5 = 0
+  if n % 3 == 0 || n % 5 == 0
     return 'FizzBuzz'
     
   if n % 3 != 0 || n % 5 != 0
